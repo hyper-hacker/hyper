4e46ad3210397c726e0f2b1c2ac6a145064de58a
AmongUsInRealLife-SUS-SUS <70657691+hyper-hacker@users.noreply.github.com>
2022-03-17 11:32:37 -0500

    Add files via upload

---
+++  code backup.js
@@ -0,0 +1,39 @@
+if(window.location.href.indexOf("cookieclicker") > -1) 
+            {
+                 alert("you are on cookie clicker");
+              window.location.href = "/gfiles/Cookie_Clicker_App/";
+            }
+if(window.location.href.indexOf("cookie-clicker") > -1) 
+            {
+                 alert("you are on cookie clicker");
+              window.location.href = "/gfiles/Cookie_Clicker_App/";
+            }
+
+let url = window.location.href
+let str = url;
+
+console.log(str.substr(str.lastIndexOf("?")+1));
+
+let newvalue = (str.substr(str.lastIndexOf("?")+1));
+
+console.log(newvalue);
+
+if (newvalue == 'inspect') {
+
+(function(){(function() {var x = document.createElement("script");x.src = "https://luphoria.com/fbl/fbl/firebug-lite-debug.js";x.onload = alert("Loaded Developer Console!");document.head.appendChild(x);})()})();
+  
+}
+
+            if(window.location.href.indexOf("play.geforcenow.com") > -1) 
+            {
+                 alert("you are on geforce now");
+              window.location.href = "https://b316b910-1659-4537-9145-91568fdc5568.id.repl.co";
+            }
+
+            if(window.location.href.indexOf("youtube.com") > -1) 
+            {
+                 alert("you are on youtube");
+              window.location.href = "https://26658572-b7d4-4971-aea1-1de5eb7cba5d.id.repl.co";
+            }
+
+//bundle.js AND document 
