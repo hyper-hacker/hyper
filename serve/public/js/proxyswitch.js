1743b9e57a7ac1ab7bef5c666a0c90bf367e46b8
AmongUsInRealLife-SUS-SUS <70657691+hyper-hacker@users.noreply.github.com>
2022-03-26 04:06:14 +0000

    attempt one

--- serve/public/js/proxyswitch.js
+++ serve/public/js/proxyswitch.js
@@ -1,15 +1,3 @@
-function corrosion {
-  
-}
-
-function womginx() {
-
-}
-
-function ultraviolet {
-  
-}
-
-function alloy {
-  
+if Cookies.get('hostcookie') = ''{
+  Cookies.set('hostcookie', 'https://7370d14d-a828-4b34-b980-c78b04c73759.id.repl.co/serve/');
 }
