8438488dbd71f3611f9b494bbcca80ad8ccaed56
AmongUsInRealLife-SUS-SUS <70657691+hyper-hacker@users.noreply.github.com>
2022-03-28 14:55:36 +0000

    backup incase attempt goes wrong

--- serve/public/js/js.js
+++ serve/public/js/js.js
@@ -1,16 +1,7 @@
-/**
- * Minified by jsDelivr using Terser v3.14.1.
- * Original file: /npm/js-cookie@2.2.1/src/js.cookie.js
- * 
- * Do NOT use SRI with dynamically generated files! More information: https://www.jsdelivr.com/using-sri-with-dynamic-files
- */
-!function(e){var n;if("function"==typeof define&&define.amd&&(define(e),n=!0),"object"==typeof exports&&(module.exports=e(),n=!0),!n){var t=window.Cookies,o=window.Cookies=e();o.noConflict=function(){return window.Cookies=t,o}}}(function(){function e(){for(var e=0,n={};e<arguments.length;e++){var t=arguments[e];for(var o in t)n[o]=t[o]}return n}function n(e){return e.replace(/(%[0-9A-Z]{2})+/g,decodeURIComponent)}return function t(o){function r(){}function i(n,t,i){if("undefined"!=typeof document){"number"==typeof(i=e({path:"/"},r.defaults,i)).expires&&(i.expires=new Date(1*new Date+864e5*i.expires)),i.expires=i.expires?i.expires.toUTCString():"";try{var c=JSON.stringify(t);/^[\{\[]/.test(c)&&(t=c)}catch(e){}t=o.write?o.write(t,n):encodeURIComponent(String(t)).replace(/%(23|24|26|2B|3A|3C|3E|3D|2F|3F|40|5B|5D|5E|60|7B|7D|7C)/g,decodeURIComponent),n=encodeURIComponent(String(n)).replace(/%(23|24|26|2B|5E|60|7C)/g,decodeURIComponent).replace(/[\(\)]/g,escape);var f="";for(var u in i)i[u]&&(f+="; "+u,!0!==i[u]&&(f+="="+i[u].split(";")[0]));return document.cookie=n+"="+t+f}}function c(e,t){if("undefined"!=typeof document){for(var r={},i=document.cookie?document.cookie.split("; "):[],c=0;c<i.length;c++){var f=i[c].split("="),u=f.slice(1).join("=");t||'"'!==u.charAt(0)||(u=u.slice(1,-1));try{var a=n(f[0]);if(u=(o.read||o)(u,a)||n(u),t)try{u=JSON.parse(u)}catch(e){}if(r[a]=u,e===a)break}catch(e){}}return e?r[e]:r}}return r.set=i,r.get=function(e){return c(e,!1)},r.getJSON=function(e){return c(e,!0)},r.remove=function(n,t){i(n,"",e(t,{expires:-1}))},r.defaults={},r.withConverter=t,r}(function(){})});
-//# sourceMappingURL=/sm/b0ce608ffc029736e9ac80a8dd6a7db2da8e1d45d2dcfc92043deb2214aa30d8.map
 let history = {};
 let sir = true;
 
 // functions
-
 function getUrl(x) {
   console.log(x)
   const urlBarcontents = x || document.getElementById('urlbar').value;
@@ -61,12 +52,12 @@ function getBookmark() {
 };
 function setinfo(aa) {
   document.getElementsByClassName(aa)[0].firstChild.data = document.getElementById(aa).contentDocument.getElementsByTagName("title")[0].firstChild.textContent;
-  if (!document.getElementById(aa).contentWindow.location.href.includes(Cookies.get('hostcookie'))) {
+  if (!document.getElementById(aa).contentWindow.location.href.includes('/service/')) {
     addPageToHistory(aa, document.getElementById(aa).contentWindow.location.href);
     return;
   }
   let regUrl = document.getElementById(aa).contentWindow.location.href;
-  regUrl = regUrl.split(Cookies.get('hostcookie')).slice(1).join(Cookies.get('hostcookie'));
+  regUrl = regUrl.split('/service/').slice(1).join('/service/');
   if (getActiveFrameId() === +aa) {
     document.getElementById('urlbar').value = decodeURIComponent(regUrl);
   }
@@ -84,18 +75,10 @@ function action(a) {
 
     
     let b64URL = getUrl();
-     console.log(b64URL)
-Cookies.set('b64URL', b64URL);
-    console.log(Cookies.get('hostcookie'))
-    console.log(getUrl)
-    document.getElementById(getActiveFrameId()).src = Cookies.get('hostcookie') + Cookies.get('b64URL');
+    document.getElementById(getActiveFrameId()).src = "/service/" + getUrl();
   } else {
-Cookies.set('b64URL', b64URL);
-    console.log(b64URL)
-    console.log(Cookies.get('hostcookie'))
     let b64URL = getUrl();
-console.log(getUrl)
-    document.getElementById(getActiveFrameId()).src = Cookies.get('hostcookie') + Cookies.get('b64URL');
+    document.getElementById(getActiveFrameId()).src = "/service/" + getUrl();
   }
 }
 function hideId(...x) {
@@ -139,23 +122,9 @@ function Fullscreen() {
 
 
 
-function corrosion() {
-Cookies.set('hostcookie', 'https://7370d14d-a828-4b34-b980-c78b04c73759.id.repl.co/serve/');
-let host = "/serve/"
-console.log(host)
-}
+function infinitecookies() {
 
-function womginx() {
-Cookies.set('hostcookie', 'https://womginxproxy.herokuapp.com/main/');
-let host = "https://womginxproxy.herokuapp.com/main/"
-console.log(host)
-}
-
-function ultraviolet() {
-  
-}
-
-function alloy() {
+  document.getElementById('2').contentWindow.location.replace("/gfiles/Cookie_Clicker_App/index.html?infcookies")
   
 }
 
@@ -245,7 +214,7 @@ function opencity(a) {
       break;
     default:
       let regUrl = document.getElementById(a).contentWindow.location.href;
-      regUrl = regUrl.split(host).slice(1).join(host);
+      regUrl = regUrl.split('/service/').slice(1).join('/service/');
       if (getActiveFrameId() === +a) {
         document.getElementById('urlbar').value = decodeURIComponent(regUrl);
       }
@@ -291,7 +260,7 @@ newTab = (url) => {
   document.getElementById('urlbar').value = url;
   var uwu = i++;
   var frame = document.createElement('IFRAME');
-  frame.setAttribute('src', 'https://' + location.host + host + url);
+  frame.setAttribute('src', 'https://' + location.host + '/service/' + url);
   frame.setAttribute('allow', 'fullscreen');
   frame.setAttribute('sandbox', 'allow-same-origin allow-scripts allow-popups allow-forms allow-pointer-lock')
   document.body.appendChild(frame);
--- serve/public/js/proxyswitch.js
+++ serve/public/js/proxyswitch.js
@@ -1,3 +1,15 @@
-if Cookies.get('hostcookie') = ''{
-  Cookies.set('hostcookie', 'https://7370d14d-a828-4b34-b980-c78b04c73759.id.repl.co/serve/');
+function corrosion {
+  
+}
+
+function womginx() {
+
+}
+
+function ultraviolet {
+  
+}
+
+function alloy {
+  
 }
