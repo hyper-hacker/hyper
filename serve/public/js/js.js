c8b444cd95916ff76c59d239e9a26b0da587fb41
AmongUsInRealLife-SUS-SUS <70657691+hyper-hacker@users.noreply.github.com>
2022-03-29 02:13:55 +0000

    everything (almost) works fine , little slow sometimes tho.

--- serve/public/js/js.js
+++ serve/public/js/js.js
@@ -1,3 +1,4 @@
+!function(e,t){"object"==typeof exports&&"undefined"!=typeof module?module.exports=t():"function"==typeof define&&define.amd?define(t):(e=e||self,function(){var n=e.Cookies,o=e.Cookies=t();o.noConflict=function(){return e.Cookies=n,o}}())}(this,(function(){"use strict";function e(e){for(var t=1;t<arguments.length;t++){var n=arguments[t];for(var o in n)e[o]=n[o]}return e}return function t(n,o){function r(t,r,i){if("undefined"!=typeof document){"number"==typeof(i=e({},o,i)).expires&&(i.expires=new Date(Date.now()+864e5*i.expires)),i.expires&&(i.expires=i.expires.toUTCString()),t=encodeURIComponent(t).replace(/%(2[346B]|5E|60|7C)/g,decodeURIComponent).replace(/[()]/g,escape);var c="";for(var u in i)i[u]&&(c+="; "+u,!0!==i[u]&&(c+="="+i[u].split(";")[0]));return document.cookie=t+"="+n.write(r,t)+c}}return Object.create({set:r,get:function(e){if("undefined"!=typeof document&&(!arguments.length||e)){for(var t=document.cookie?document.cookie.split("; "):[],o={},r=0;r<t.length;r++){var i=t[r].split("="),c=i.slice(1).join("=");try{var u=decodeURIComponent(i[0]);if(o[u]=n.read(c,u),e===u)break}catch(e){}}return e?o[e]:o}},remove:function(t,n){r(t,"",e({},n,{expires:-1}))},withAttributes:function(n){return t(this.converter,e({},this.attributes,n))},withConverter:function(n){return t(e({},this.converter,n),this.attributes)}},{attributes:{value:Object.freeze(o)},converter:{value:Object.freeze(n)}})}({read:function(e){return'"'===e[0]&&(e=e.slice(1,-1)),e.replace(/(%[\dA-F]{2})+/gi,decodeURIComponent)},write:function(e){return encodeURIComponent(e).replace(/%(2[346BF]|3[AC-F]|40|5[BDE]|60|7[BCD])/g,decodeURIComponent)}},{path:"/"})}));
 let history = {};
 let sir = true;
 
@@ -52,12 +53,18 @@ function getBookmark() {
 };
 function setinfo(aa) {
   document.getElementsByClassName(aa)[0].firstChild.data = document.getElementById(aa).contentDocument.getElementsByTagName("title")[0].firstChild.textContent;
-  if (!document.getElementById(aa).contentWindow.location.href.includes('/service/')) {
+  if (!document.getElementById(aa).contentWindow.location.href.includes(Cookies.get('hostcookie') // => 'value'
+
+)) {
     addPageToHistory(aa, document.getElementById(aa).contentWindow.location.href);
     return;
   }
   let regUrl = document.getElementById(aa).contentWindow.location.href;
-  regUrl = regUrl.split('/service/').slice(1).join('/service/');
+  regUrl = regUrl.split(Cookies.get('hostcookie') // => 'value'
+
+).slice(1).join(Cookies.get('hostcookie') // => 'value'
+
+);
   if (getActiveFrameId() === +aa) {
     document.getElementById('urlbar').value = decodeURIComponent(regUrl);
   }
@@ -75,10 +82,12 @@ function action(a) {
 
     
     let b64URL = getUrl();
-    document.getElementById(getActiveFrameId()).src = "/service/" + getUrl();
+    document.getElementById(getActiveFrameId()).src = Cookies.get('hostcookie') + getUrl();
+    console.log("Loading Page...")
   } else {
     let b64URL = getUrl();
-    document.getElementById(getActiveFrameId()).src = "/service/" + getUrl();
+    document.getElementById(getActiveFrameId()).src = Cookies.get('hostcookie') + getUrl();
+    console.log("Loading Page...")
   }
 }
 function hideId(...x) {
@@ -214,7 +223,11 @@ function opencity(a) {
       break;
     default:
       let regUrl = document.getElementById(a).contentWindow.location.href;
-      regUrl = regUrl.split('/service/').slice(1).join('/service/');
+      regUrl = regUrl.split(Cookies.get('hostcookie') // => 'value'
+
+).slice(1).join(Cookies.get('hostcookie') // => 'value'
+
+);
       if (getActiveFrameId() === +a) {
         document.getElementById('urlbar').value = decodeURIComponent(regUrl);
       }
@@ -260,7 +273,9 @@ newTab = (url) => {
   document.getElementById('urlbar').value = url;
   var uwu = i++;
   var frame = document.createElement('IFRAME');
-  frame.setAttribute('src', 'https://' + location.host + '/service/' + url);
+  frame.setAttribute('src', 'https://' + location.host + Cookies.get('hostcookie') // => 'value'
+
+ + url);
   frame.setAttribute('allow', 'fullscreen');
   frame.setAttribute('sandbox', 'allow-same-origin allow-scripts allow-popups allow-forms allow-pointer-lock')
   document.body.appendChild(frame);
